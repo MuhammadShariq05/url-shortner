bf69970a67db7e0635239830d6330daa5f4e9b84
MuhammadShariq05 <shariqwrk12023@gmail.com>
2024-04-24 11:09:50 +0530

    bugfix + added url redirect

--- controllers/url.js
+++ controllers/url.js
@@ -3,17 +3,53 @@ const URL = require("../models/url");
 
 async function handleGenerateNewShortURL(req, res) {
   const body = req.body;
-  if (!body.url) return res.status(400).json({ msg: "require url" });
-  const shortId = shortid(8);
+  if (!body.url) return res.status(400).json({ msg: "URL is required" });
 
-  await URL.create({
-    shortId: shortId,
-    redirectURL: body.url,
-    visitHistory: [],
+  const shortId = shortid.generate();
+  try {
+    await URL.create({
+      shortId: shortId,
+      redirectURL: body.url,
+      visitHistory: [],
+    });
+    return res.json({ id: shortId });
+  } catch (error) {
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+}
+
+async function makeIdFromShortID(req, res) {
+  const shortId = req.params.shortId;
+  try {
+    const entry = await URL.findOneAndUpdate(
+      { shortId },
+      { $push: { visitHistory: { timestamp: Date.now() } } },
+      { new: true } // Return the updated document
+    );
+
+    if (!entry) {
+      return res.status(404).json({ msg: "URL not found" });
+    }
+
+    return res.redirect(entry.redirectURL);
+  } catch (error) {
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+}
+
+async function handleGetAnalytics(req, req) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({
+    shortId,
   });
-  return res.json({ id: shortId });
+  return res.json({
+    totalClicks: result.visitHistory.lenght,
+    analytics: result.visitHistory,
+  });
 }
 
 module.exports = {
   handleGenerateNewShortURL,
+  makeIdFromShortID,
+  handleGetAnalytics,
 };
--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const {handleGenerateNewShortURL} = require("../controllers/url");
+const {handleGenerateNewShortURL, makeIdFromShortID, handleGetAnalytics} = require("../controllers/url");
+
+
 
 const router = express.Router();
 router.post('/', handleGenerateNewShortURL);
+router.get("/:shortId", makeIdFromShortID);
+router.get("/analytics/:shortId", handleGetAnalytics);
 
 module.exports = router;
